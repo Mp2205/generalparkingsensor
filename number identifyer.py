b5b2478ad10b8cdc87c278c27e1ed22477e42a7f
Mp2205 <105429568+Mp2205@users.noreply.github.com>
2023-04-18 15:40:47 +0530

    Create number identifyer.py

---
+++ number identifyer.py
@@ -0,0 +1,14 @@
+number=int(input("Enter number1 =  "))
+
+if number>=1:
+    print("Positive")
+    print("Done")
+elif number==0:
+        print("Zero")
+        print("Done")
+elif number<= -1:
+        print("negative")
+        print("Done")
+else:
+    print("invalid")
+    print("Done")
