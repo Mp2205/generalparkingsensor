678c3fa3c2a19d78f326abf25744eece83fb1a52
Mp2205 <105429568+Mp2205@users.noreply.github.com>
2023-04-18 15:50:23 +0530

    Create passing.py

---
+++ passing.py
@@ -0,0 +1,12 @@
+"""
+Physics >> 33 and Chemistry >> 33 then Print pass else Print Fail
+"""
+
+physics=int(input("Enter physics Marks = "))
+chemistry=int(input("Enter chemistry Marks = "))
+
+
+if physics>=33 and chemistry>=33:
+    print("pass")
+else:
+    print("Fail")
